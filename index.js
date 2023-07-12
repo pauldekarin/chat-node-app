e22494cadb8992e5b77040259783847436de2f31
pauldekarin <133903099+pauldekarin@users.noreply.github.com>
2023-07-12 20:38:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const WebSocket = require('ws');
+var connected = [];
 
 const wss = new WebSocket.Server({port:8080});
 
 wss.on('connection',ws => {
 	var username = '';
-	console.log('[server] connected client...');
-	ws.on('message',message=>{
-		if (username == '') username = JSON.parse(message)['username'];
 	
+	
+	console.log('[server] connected client...');
+	
+	
+	ws.on('message',message=>{
+		var _msg = JSON.parse(message);
+		if (_msg['event'] == 'login'){
+			connected.push(_msg['username']);
+			message['connected'] = connected;
+			wss.clients.forEach(client =>{
+				if (client.readyState == WebSocket.OPEN){
+					client.send(message, {binary: WebSocket.binary});
+				}
+			});
+			return;
+		}
+		if (username == '') {
+			username = JSON.parse(message)['username'];
+		}
+		
 		wss.clients.forEach(client => {
 			if (client.readyState == WebSocket.OPEN && !(ws == client)){
 				client.send(message, {binary : WebSocket.binary});
