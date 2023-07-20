df998303b2e7beea1db6f856e91693d294494e51
pauldekarin <133903099+pauldekarin@users.noreply.github.com>
2023-07-20 21:35:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let WebSocket = require('ws');
 let wss = new WebSocket.Server({port:8080});
-wss.connected = array();
+wss.connected = [];
 
 wss.on('connection',ws => {
 	ws.on('message',message=>{
@@ -10,7 +10,7 @@ wss.on('connection',ws => {
 			ws.username = _msg['username'];
 			wss.connected.push(_msg['username']);
 			_msg['connected'] = wss.connected;
-
+			
 			wss.clients.forEach(client =>{
 				if (client.readyState == WebSocket.OPEN){
 					client.send(JSON.stringify(_msg), {binary: WebSocket.binary});
@@ -28,6 +28,22 @@ wss.on('connection',ws => {
 			return;
 		}
 
+		if (_msg['event'] == 'receivedSlide'){
+			if (ws.readyState == WebSocket.OPEN){
+				wss.clients.forEach(client => {
+					console.log(_msg.sender, client.username);
+					if (client.username == _msg.sender){
+						client.send(JSON.stringify({
+							event:'receivedSlide',
+							username:ws.username
+						}))
+					}
+					return;
+				})
+			}
+			return;
+		}
+
 		wss.clients.forEach(client => {
 			if (client.readyState == WebSocket.OPEN && !(ws == client)){
 				client.send(message, {binary : WebSocket.binary});
@@ -36,17 +52,18 @@ wss.on('connection',ws => {
 	})
 
 	ws.on('close', function (message) {
-	  wss.connected.splice(wss.connected.indexOf(ws.username),1);
-	  wss.clients.forEach(function each (client) {
-	    if (client !== ws && client.readyState === WebSocket.OPEN) {
-			client.send(JSON.stringify({
-				username : ws.username,
-				event: 'logout',
-				connected:wss.connected
-	      }))
-	    }
-	  })
+		if (typeof ws.username != 'undefined'){
+			wss.connected.splice(wss.connected.indexOf(ws.username),1);
+			wss.clients.forEach(function each (client) {
+				if (client !== ws && client.readyState === WebSocket.OPEN) {
+					client.send(JSON.stringify({
+						username : ws.username,
+						event: 'logout',
+						connected:wss.connected
+				  }))
+				}
+			})
+		}
 	})
 
 })
-
